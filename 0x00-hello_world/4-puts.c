c804c58ecef5dd99dc2f8dcab9662c0344730684
HenryMartins <henryabung955@gmail.com>
2022-09-25 17:36:28 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
 /**
- * main is the enntry point
+ * main is the entry point
  *
- * returns 0 by default
- *
+ * return :0
  */
 
- int main(void)
- {
+int main(void)
+{
 		puts("\"programming is like building a multilingual puzzle");
 			return (0);
- }			
- 				
+}			
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main is the entry point
+ *
+ * return 0
+ */
+
+int main(void)
+{
+                       printf("with the proper grammar, but the out come is a peice of art, /n");
+		       return (0);
+}		       
